bf2faa1657e51122ae1987fa3c39f568e37a94bf
Long HOANG <longhoang@hotmail.fr>
2014-04-14 19:53:41 +0200

    Mise à jour de la bdd gestionrh.sql.

--- gestionrh.sql
+++ gestionrh.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Lun 17 Février 2014 à 12:16
--- Version du serveur: 5.5.24-log
--- Version de PHP: 5.3.13
+-- Généré le: Lun 14 Avril 2014 à 17:48
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,35 +19,70 @@ SET time_zone = "+00:00";
 --
 -- Base de données: `gestionrh`
 --
-DROP DATABASE IF EXISTS `gestionrh`;
 CREATE DATABASE IF NOT EXISTS `gestionrh` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `gestionrh`;
-
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `authentifiant`
+-- Structure de la table `candidat`
 --
 
-CREATE TABLE IF NOT EXISTS `authentifiant` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `login` varchar(20) DEFAULT NULL,
-  `password` varchar(40) DEFAULT NULL,
-  `email` varchar(30) NOT NULL,
-  `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+CREATE TABLE IF NOT EXISTS `candidat` (
+  `idCandidat` int(11) NOT NULL AUTO_INCREMENT,
+  `posteSouhaite` varchar(25) DEFAULT NULL,
+  `nomCandidat` varchar(25) DEFAULT NULL,
+  `prenomCandidat` varchar(30) DEFAULT NULL,
+  `emailCandidat` varchar(30) DEFAULT NULL,
+  `diplomesCandidat` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`idCandidat`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Contenu de la table `authentifiant`
+-- Structure de la table `candidature`
 --
 
-INSERT INTO `authentifiant` (`id`, `login`, `password`, `email`, `dateCreation`) VALUES
-(1, 'long', '8e9e073e70db91b9353a7b2919f86642e743d9f5', '', '0000-00-00 00:00:00'),
-(2, 'adrien', '17d9cbc5f47611875ad06482b0b570d84b24278a', '', '0000-00-00 00:00:00'),
-(3, 'preston', '29c451be644da75cfd0a906bb8644f4bd8f3ef49', '', '0000-00-00 00:00:00'),
-(4, 'sophie', '7cb7e21b9bd99b6970b22dea8c90a508cae673b2', '', '0000-00-00 00:00:00');
+CREATE TABLE IF NOT EXISTS `candidature` (
+  `idCandidature` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCandidature` date DEFAULT NULL,
+  `etatCandidature` varchar(25) DEFAULT NULL,
+  `idPoste` int(11) DEFAULT NULL,
+  `idCandidat` int(11) DEFAULT NULL,
+  `idSalarie` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idCandidature`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conge`
+--
+
+CREATE TABLE IF NOT EXISTS `conge` (
+  `motifConge` varchar(25) DEFAULT NULL,
+  `commentaireConge` varchar(25) DEFAULT NULL,
+  `idDemande` int(11) NOT NULL,
+  PRIMARY KEY (`idDemande`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `demande`
+--
+
+CREATE TABLE IF NOT EXISTS `demande` (
+  `idDemande` int(11) NOT NULL AUTO_INCREMENT,
+  `dateDemande` date DEFAULT NULL,
+  `dateValidationDemande` date DEFAULT NULL,
+  `dateDebutFormation_Conge` date DEFAULT NULL,
+  `dureeFormation_Conge` varchar(25) DEFAULT NULL,
+  `idSalarie` int(11) DEFAULT NULL,
+  `idSalarie_1` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idDemande`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -56,10 +91,22 @@ INSERT INTO `authentifiant` (`id`, `login`, `password`, `email`, `dateCreation`)
 --
 
 CREATE TABLE IF NOT EXISTS `employe` (
-  `idEmploye` int(8) NOT NULL AUTO_INCREMENT,
-  `typeEmploye` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`idEmploye`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `idSalarie` int(11) NOT NULL,
+  PRIMARY KEY (`idSalarie`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formation`
+--
+
+CREATE TABLE IF NOT EXISTS `formation` (
+  `libelleFormation` varchar(25) DEFAULT NULL,
+  `niveauFormation` varchar(25) DEFAULT NULL,
+  `idDemande` int(11) NOT NULL,
+  PRIMARY KEY (`idDemande`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -68,9 +115,54 @@ CREATE TABLE IF NOT EXISTS `employe` (
 --
 
 CREATE TABLE IF NOT EXISTS `interimaire` (
-  `idInterimaire` int(8) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`idInterimaire`)
+  `dateFinContrat` date DEFAULT NULL,
+  `idSalarie` int(11) NOT NULL,
+  PRIMARY KEY (`idSalarie`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `planning`
+--
+
+CREATE TABLE IF NOT EXISTS `planning` (
+  `idPlanning` int(11) NOT NULL AUTO_INCREMENT,
+  `jourPlanning` varchar(25) DEFAULT NULL,
+  `moisPlanning` varchar(25) DEFAULT NULL,
+  `anneePlanning` varchar(25) DEFAULT NULL,
+  `heurePlanning` varchar(10) DEFAULT NULL,
+  `idSalarie` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPlanning`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `poste`
+--
+
+CREATE TABLE IF NOT EXISTS `poste` (
+  `idPoste` int(11) NOT NULL AUTO_INCREMENT,
+  `libellePoste` varchar(25) DEFAULT NULL,
+  `lieuPoste` varchar(25) DEFAULT NULL,
+  `typePoste` varchar(25) DEFAULT NULL,
+  `descriptifPoste` text,
+  PRIMARY KEY (`idPoste`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rh`
+--
+
+CREATE TABLE IF NOT EXISTS `rh` (
+  `datePriseFonctionRh` date DEFAULT NULL,
+  `mdpRh` varchar(40) DEFAULT NULL,
+  `idSalarie` int(11) NOT NULL,
+  PRIMARY KEY (`idSalarie`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -79,22 +171,20 @@ CREATE TABLE IF NOT EXISTS `interimaire` (
 --
 
 CREATE TABLE IF NOT EXISTS `salarie` (
-  `idSalarie` int(8) NOT NULL AUTO_INCREMENT,
+  `idSalarie` int(11) NOT NULL AUTO_INCREMENT,
+  `emailSalarie` varchar(30) DEFAULT NULL,
+  `mdpSalarie` varchar(40) DEFAULT NULL,
   `nomSalarie` varchar(20) DEFAULT NULL,
   `prenomSalarie` varchar(20) DEFAULT NULL,
   `dateNaissanceSalarie` date DEFAULT NULL,
-  `lieuNaissanceSalarie` varchar(40) DEFAULT NULL,
-  `sexeSalarie` varchar(1) DEFAULT NULL,
-  `groupeSanguinSalarie` varchar(3) DEFAULT NULL,
-  `situationFamilialeSalarie` varchar(30) DEFAULT NULL,
-  `adresseSalarie` varchar(50) DEFAULT NULL,
-  `telSalarie` varchar(12) DEFAULT NULL,
-  `emailSalarie` varchar(20) DEFAULT NULL,
-  `numBadgeSalarie` varchar(10) DEFAULT NULL,
-  `zoneAutoriseeSalarie` varchar(10) DEFAULT NULL,
-  `salaireSalarie` float(8,2) DEFAULT NULL,
-  `niveauScolaireSalarie` varchar(10) DEFAULT NULL,
-  `libelleServiceSalarie` varchar(20) DEFAULT NULL,
+  `sexeSalarie` char(1) DEFAULT NULL,
+  `adresseSalarie` varchar(20) DEFAULT NULL,
+  `telSalarie` int(11) DEFAULT NULL,
+  `diplomesSalarie` varchar(30) DEFAULT NULL,
+  `salaireSalarie` float DEFAULT NULL,
+  `dateRecrutementSalarie` date DEFAULT NULL,
+  `idPlanning` int(11) NOT NULL,
+  `idPoste` int(11) DEFAULT NULL,
   PRIMARY KEY (`idSalarie`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
@@ -102,11 +192,11 @@ CREATE TABLE IF NOT EXISTS `salarie` (
 -- Contenu de la table `salarie`
 --
 
-INSERT INTO `salarie` (`idSalarie`, `nomSalarie`, `prenomSalarie`, `dateNaissanceSalarie`, `lieuNaissanceSalarie`, `sexeSalarie`, `groupeSanguinSalarie`, `situationFamilialeSalarie`, `adresseSalarie`, `telSalarie`, `emailSalarie`, `numBadgeSalarie`, `zoneAutoriseeSalarie`, `salaireSalarie`, `niveauScolaireSalarie`, `libelleServiceSalarie`) VALUES
-(1, 'HOANG', 'Long', '1988-06-23', 'Levallois-Perret', 'H', 'O+', 'CÃ©libataire', '65 avenue du GÃ©nÃ©ral de Gaulle', '0618531032', 'longhoang@hotmail.fr', '007', 'A', 0.00, 'Bac+1', 'Videur'),
-(2, 'MORLA', 'Adrien', '0000-00-00', '', 'H', '', 'CÃ©libataire', '', '', '', '', NULL, 0.00, 'Bac+1', 'Physicien des herbes'),
-(3, 'SIGER', 'Preston', '0000-00-00', '', 'H', '', 'FiancÃ©', '', '', '', '', '', 0.00, 'Bac+1', 'Ultra IngÃ©nieur'),
-(4, 'NGUYEN', 'Sophie', '0000-00-00', '', 'F', '', '', '', '', '', '', 'A', NULL, 'Bac+5', 'Super Woman');
+INSERT INTO `salarie` (`idSalarie`, `emailSalarie`, `mdpSalarie`, `nomSalarie`, `prenomSalarie`, `dateNaissanceSalarie`, `sexeSalarie`, `adresseSalarie`, `telSalarie`, `diplomesSalarie`, `salaireSalarie`, `dateRecrutementSalarie`, `idPlanning`, `idPoste`) VALUES
+(1, 'longhoang@hotmail.fr', '8e9e073e70db91b9353a7b2919f86642e743d9f5', 'HOANG', 'Long', '1988-06-23', 'H', '', 0, '', 0, '2014-04-02', 0, NULL),
+(2, 'adrien.morla66@gmail.com', '17d9cbc5f47611875ad06482b0b570d84b24278a', 'MORLA', 'Adrien', '0000-00-00', 'H', '', 0, '', 0, '2014-04-02', 0, NULL),
+(3, 'sigerpreston@hotmail.com', '29c451be644da75cfd0a906bb8644f4bd8f3ef49', 'SIGER', 'Preston', '0000-00-00', 'H', '', 0, '', 0, '0000-00-00', 0, NULL),
+(4, '', '', 'NGUYEN', 'Sophie', '0000-00-00', 'F', '', 0, '', 0, '0000-00-00', 0, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
