38177fa3462db4ad80fe89536d1eec06d14830e7
Adrien78 <adrien.morla66@gmail.com>
2014-04-16 16:20:14 +0200

    mise à jours planning

--- admin/planning.php
+++ admin/planning.php
@@ -4,41 +4,67 @@
 	include_once"../include/header.php";
 	include_once"../include/navbar.php";
 ?>
-<!-- Menu -->
-<div class="container">
-	<div class ="row">
-		<legend>Plannings des salariés</legend>
-		<ul class="nav nav-pills nav-stacked">
-		</ul>
-	</div>
-</div>
 
 <!-- Contenu -->
-<!-- L'affichage dois ce présenter sous la forme d'un calendirer avec les heures et les jours (comme un agenda). 
-Quand on rentre l'ID d'un salarié, son planning ce génère automatiquement en fonction de ses heures de travaiiles-->
-	<div class="span9">
-		<legend>EN TRAVAUX !</legend>
-			<table>
-				<tr>
-					<?php
-						$reponse = $bdd->query('SELECT * FROM planning');
-						while($donnees = $reponse->fetch()){
-					?>
-					<p>
-						<td><b>Jour :</b> <?php echo htmlspecialchars($donnees['jourPlanning']);?><br/></td>
-						<td><b>Mois :</b> <?php echo htmlspecialchars($donnees['moisPlanning']);?><br/></td>
-						<td><b>Année :</b> <?php echo htmlspecialchars($donnees['anneePlanning']);?><br/></td>
-						<td><b>Heure :</b> <?php echo htmlspecialchars($donnees['heurePlanning']);?><br/></td>
-						<td><b>Numéro de salarié :</b> <?php echo htmlspecialchars($donnees['idSalarie']);?><br/></td>
-					</p>
-					<?php
-						}
-						$reponse->closeCursor();
-					?>
-				</tr>
-			</table>
-			<a href="accueil.php" class="btn btn-primary">Retour à l'accueil</a>		
-	</div>
+<!-- L'affichage doit se présenter sous la forme d'un premier calendirer avec les mois, semaines(jours) et l'année correspondante. 
+Quand on rentre l'ID d'un salarié, son planning se génère automatiquement puis on peut accéder au planning détailler (second planning) qui renseigne les horraires de travaille chaque jours-->
+		
+<div class="container">
+	<legend>Plannings des salariés</legend>
+	<?php
+		if(isset($_POST['requete']) && $_POST['requete'] != NULL) // on vérifie d'abord l'existence du POST et aussi si la requete n'est pas vide.
+		{
+			mysql_connect('localhost','root','');
+			mysql_select_db('gestionrh'); // on se connecte à MySQL. Je vous laisse remplacer les différentes informations pour adapter ce code à votre site.
+			$requete = htmlspecialchars($_POST['requete']); // on crée une variable $requete pour faciliter l'écriture de la requête SQL, mais aussi pour empêcher les éventuels malins qui utiliseraient du PHP ou du JS, avec la fonction htmlspecialchars().
+			$query = mysql_query("SELECT * FROM salarie WHERE nomSalarie LIKE '%$requete%' ORDER BY idSalarie DESC") or die (mysql_error()); // la requête, que vous devez maintenant comprendre ;)
+			$nb_resultats = mysql_num_rows($query); // on utilise la fonction mysql_num_rows pour compter les résultats pour vérifier par après
+		if($nb_resultats != 0) // si le nombre de résultats est supérieur à 0, on continue
+		{
+			// maintenant, on va afficher les résultats et la page qui les donne ainsi que leur nombre, avec un peu de code HTML pour faciliter la tâche.
+			?>
+			<h3>Résultats de votre recherche.</h3>
+			<p>Nous avons trouvé <?php echo $nb_resultats; // on affiche le nombre de résultats 
+			if($nb_resultats > 1) { echo 'résultats'; } else { echo 'résultat'; } // on vérifie le nombre de résultats pour orthographier correctement. 
+			?>
+			 dans notre base de données. Voici les Salariés que nous avons trouvées :<br/>
+			<br/>
+			<?php
+			while($donnees = mysql_fetch_array($query)) // on fait un while pour afficher la liste des fonctions trouvées, ainsi que l'id qui permettra de faire le lien vers la page de la fonction
+			{
+			?>
+			<a href="planning.php?id=<?php echo $donnees['idSalarie']; ?>"><?php echo $donnees['nomSalarie']; ?></a><br/>
+			<?php
+			} // fin de la boucle
+			?><br/>
+			<br/>
+			<a href="planning.php">Faire une nouvelle recherche</a></p>
+			<?php
+		} // Fini d'afficher les résultats ! Maintenant, nous allons afficher l'éventuelle erreur en cas d'échec de recherche et le formulaire.
+		else
+		{ // de nouveau, un peu de HTML
+			?>
+			<h3>Pas de résultats</h3>
+			<p>Nous n'avons trouvé aucun résultat pour votre requête "<? echo $_POST['requete']; ?>". <a href="rechercher.php">Réessayez</a> avec autre chose.</p>
+			<?php
+		}// Fini d'afficher l'erreur ^^
+		mysql_close(); // on ferme mysql, on n'en a plus besoin
+		}
+		else
+		{ // et voilà le formulaire, en HTML de nouveau !
+			?>
+
+			<p>Vous allez faire une recherche dans notre base de données concernant les Salariés. Tapez une requête pour réaliser une recherche.</p>
+			<form action="planning.php" method="Post">
+				<input type="text" name="requete" size="10"/><br/><br/>
+				<input type="submit" value="Ok"/>
+			</form>
+			<?php
+		}
+		// et voilà, c'est fini !
+	?>
+	<a href="accueil.php" class="btn btn-primary">Retour à l'accueil</a>		
+</div>
 <?php 
 	include_once"../include/javascript.php";
 	include_once"../include/footer.php"; 
