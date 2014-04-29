a73bde51d03408fab189bc232b823c941ddee747
Long HOANG <longhoang@hotmail.fr>
2014-04-29 15:52:39 +0200

    Suppression de la copie de bootstrap.css.

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-	if (!empty($_GET['page']) && is_file('controleurs/Cnt'.$_GET['page'].'.php')){
+	if(!empty($_GET['page']) && is_file('controleurs/Cnt'.$_GET['page'].'.php'))
+	{
 		require_once 'controleurs/Cnt'.$_GET['page'].'.php';
 	}
-	else{
-	require_once 'controleurs/CntIndex.php';
+	else
+	{
+		require_once 'controleurs/CntIndex.php';
 	}
 ?>
