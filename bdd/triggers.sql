2c2fe4ff5782732f1103df9fea6b2d1259ab7e69
Preston <sigerpreston@hotmail.fr>
2014-05-05 21:23:46 +0200

    Triggers sql et table candidat_histo

--- bdd/triggers.sql
+++ bdd/triggers.sql
@@ -1,5 +1,61 @@
--- Mettre les tirggers ici
+-- Mettre les triggers ici
 
-USE gestionrh
+CREATE TABLE candidat_histo (
+  `idCandidat` int(11) NOT NULL AUTO_INCREMENT,  -- Colonnes Historisées
+  `nomCandidat` varchar(25) DEFAULT NULL,
+  `prenomCandidat` varchar(30) DEFAULT NULL,
+  `commentaires` TEXT,
+  
+  date_histo DATETIME NOT NULL,                   -- Colonnes techniques
+  utilisateur_histo VARCHAR(20) NOT NULL,
+  evenement_histo CHAR(6) NOT NULL,
+  PRIMARY KEY (idCandidat, date_histo)
+) ENGINE=InnoDB;
 
-CREATE TABLE candidat
+DELIMITER //
+CREATE TRIGGER after_update_candidat 
+AFTER UPDATE ON candidat 
+FOR EACH ROW
+BEGIN
+    INSERT INTO candidat_histo (
+        idCandidat, 
+        nomCandidat, 
+        prenomCandidat,  
+        commentaires, 
+        date_histo, 
+        utilisateur_histo, 
+        evenement_histo)
+    VALUES (
+        OLD.idCandidat,
+        OLD.nomCandidat,
+		OLD.prenomCandidat,
+        OLD.commentaires,
+
+        NOW(),
+        CURRENT_USER(),
+        'UPDATE');
+END //
+
+CREATE TRIGGER after_delete_candidat 
+AFTER DELETE ON candidat 
+FOR EACH ROW
+BEGIN
+        INSERT INTO candidat_histo (
+        idCandidat, 
+        nomCandidat, 
+        prenomCandidat,  
+        commentaires, 
+        date_histo, 
+        utilisateur_histo, 
+        evenement_histo)
+    VALUES (
+        OLD.idCandidat,
+        OLD.nomCandidat,
+		OLD.prenomCandidat,
+        OLD.commentaires,
+
+        NOW(),
+        CURRENT_USER(),
+        'UPDATE');
+END //
+DELIMITER ;
